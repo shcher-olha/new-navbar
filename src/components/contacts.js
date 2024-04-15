6e3c86f86a93b566b3c9e7dea6169365f7bb70de
shcher-olha <shcher.olha@gmail.com>
2024-04-15 20:03:54 +0200

    create contacts.js

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,13 +1,33 @@
-
+import React from "react";
+import "../components/contacts.css";
+import rollsImage from "../images/rolls.jpg";
 
 const Contacts = () => {
   return (
     <>
-    <h1>Contacts HHHHHHHHHHHH</h1>
-<button type="button" className="btn btn-primary">
-  Primary
-</button>
-  
+      <div className="container">
+        <h1>Contacts</h1>
+        <div className="row">
+          <div className="col-6">
+            <img src={rollsImage} alt="Rolls" className="image-rolls" />
+          </div>
+          <div className="col-6">
+            <p className="text">
+              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently
+              with desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </p>
+          </div>
+        </div>
+      </div>
+      
     </>
   );
 };
